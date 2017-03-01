6b21b8196bf7df91600c01d267f4bb7cb0408bf7
Zac Blanco <zblanco@apache.org>
2017-02-28 20:46:01 -0500

    sample require_role on stat dashboard
    Sample code to show how require role is used.

--- chefboyrd/views/stat_dash.py
+++ chefboyrd/views/stat_dash.py
@@ -6,13 +6,14 @@ of the Point of sale system and other data systems for the business.
 '''
 from flask import Blueprint, render_template, abort
 from jinja2 import TemplateNotFound
-from flask_login import login_required
+from chefboyrd.auth import require_role
 
-page = Blueprint('dashboard', __name__, template_folder='templates')
+page = Blueprint('dashboard', __name__, template_folder='./templates')
 
 @page.route("/")
-@login_required
+@require_role('admin') # Example of requireing a role(and authentication)
 def dash_index():
     '''Renders the index page of the dashboards
     '''
-    render_template('dashboard/index.html')
+    # Logged in always true because we require admin role
+    return render_template('/dashboard/index.html', logged_in=True)
