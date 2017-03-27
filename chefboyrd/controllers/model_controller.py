d4e031b905dc3e2e92c27f2adc1ad11bc5b8cfc7
Richard Ahn <richahn2@gmail.com>
2017-03-27 00:55:48 -0400

    Added prediction

--- chefboyrd/controllers/model_controller.py
+++ chefboyrd/controllers/model_controller.py
@@ -4,28 +4,98 @@ our local models, it converts it into numbers usable by the prediction controlle
 
 '''
 from peewee import Model
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.optimize import curve_fit
+from datetime import datetime, timedelta, date, time
+from chefboyrd.models import Orders, Tabs, Meals, Ingredients, MealIngredients, Quantities
+from peewee import fn
 
+def polynomialModel(x, *params):
+    complexity = 1
+    sum = params[0]
+    for i in range(len(x)):
+        for j in range(1,complexity+1):
+            sum += params[complexity*i+j]*(x[i]**j)
+    return sum
 
-def objects_to_list():
+def sinusoidalModel(x, *params):
+    sum = params[9]
+    for i in range(len(x)):
+        sum += params[3*i]*np.sin(params[3*i+1]*x[i]+params[3*i+2])
+    return sum
+
+def orders_to_list(orders):
     '''Converts peewee query result set to a list of dictionary of lists'''
-    pass
+    orders = orders.switch(Orders).join(Meals).order_by(Meals.name)
+    currentMeal = ""
+    previousMeal = ""
+    currentHour = -1
+    previousHour = -1
+    mealCounter = 0
+    mealDict = {}
+    for order in orders:
+        currentMeal = order.meal.name
+        # Check if you're on a new meal
+        if currentMeal != previousMeal:
+            # Add the rest of the mealCounter to the previous meal
+            if previousMeal != "" and mealCounter != 0:
+                ts = order.tab.timestamp
+                row = np.array([[ts.hour, ts.day, ts.month, mealCounter]])
+                if previousMeal in mealDict:
+                    mealDict[previousMeal] = np.append(mealDict[previousMeal], row, axis=0)
+                else:
+                    mealDict[previousMeal] = row
+                mealCounter = 0
 
-def scale_data():
-    '''Scale data over a min/max range'''
-    pass
+        mealCounter += 1
+        # Check if the hour bucket is the same
+        currentHour = order.tab.timestamp.hour 
+        if currentHour != previousHour:
+            if previousHour != -1:
+                ts = order.tab.timestamp
+                row = np.array([[ts.hour, ts.day, ts.month, mealCounter]])
+                if currentMeal in mealDict:
+                    mealDict[currentMeal] = np.append(mealDict[currentMeal], row, axis=0)
+                else:
+                    mealDict[currentMeal] = row
+                mealCounter = 0
+        previousHour = currentHour
+        previousMeal = currentMeal
+    return mealDict
 
-def train_regression():
-    '''Train and store a regression model with parameters'''
-    pass
+def train_regression(mealDict, modelType):
+    mealsParams = {}
+    for meal_key in mealDict:
+        x = np.transpose(mealDict[meal_key][:, 0:3])
+        y = mealDict[meal_key][:, 3]
+        mealParams = train_regression_single(x, y, modelType)
+        mealsParams[meal_key] = mealParams;
+    return mealsParams
+
+def train_regression_single(x, y, modelType):
+    '''Train regression model with parameters'''
+    # Train the model
+    initial_params = []
+
+    if modelType == 'Polynomial':
+        initial_params = [1.,1.,1.,1.]
+        params, pcov = curve_fit(polynomialModel, x, y, initial_params)
+        return params
+    elif modelType == 'Sinusoidal':
+        initial_params = [1.,1.,1.,1.,1.,1.,1.,1.,1.,1.]
+        params, pcov = curve_fit(sinusoidalModel, x, y, initial_params)
+        return params
 
 def get_earliest_datetime():
     '''Finds the minimum datetime'''
-    pass
+    earliestDatetime = Tabs.select(fn.MIN(Tabs.timestamp)).scalar(convert=True)
+    return earliestDatetime
 
 def get_last_datetime():
     '''Finds the maximum datetime in a list'''
-    pass
+    lastDatetime = Tabs.select(fn.MAX(Tabs.timestamp)).scalar(convert=True)
+    return lastDatetime
 
-def get_last_regression_params():
-    '''Retrieves the last set of regression parameters trained.'''
-    pass
+
+    
---
+++ chefboyrd/views/prediction_dash.py
@@ -0,0 +1,73 @@
+''' Prediction dashboard for the manager interface
+
+
+Will allow you to predict ingredient usage between two chosen dates
+'''
+import urllib, base64
+from datetime import datetime, timedelta
+import logging
+from io import BytesIO
+from flask import Blueprint, render_template, abort, request, flash
+from jinja2 import TemplateNotFound
+from peewee import JOIN_LEFT_OUTER, JOIN
+from chefboyrd.auth import require_role
+from chefboyrd.models import Orders, Meals, Ingredients, Tabs, MealIngredients
+from chefboyrd.controllers import data_controller
+from chefboyrd.controllers import model_controller
+from chefboyrd.controllers import prediction_controller
+import dateutil.parser
+
+logger = logging.getLogger(__name__)
+
+
+page = Blueprint('prediction', __name__, template_folder='./templates')
+
+@page.route("/", methods=['GET', 'POST'])
+@require_role('admin')
+def prediction_index():
+	'''Renders the index page of the prediction page
+	'''
+	beginningDate = None
+	endingDate = None
+	mealUsage = None
+	if request.method == "POST":
+		try:
+			print(request.form.get('beginningDate'))
+			beginningDate = dateutil.parser.parse(request.form.get('beginningDate'))
+		except BaseException as er:
+			logger.warning(er)
+		try:
+			print(request.form.get('endingDate'))
+			endingDate = dateutil.parser.parse(request.form.get('endingDate'))
+		except BaseException as er:
+			logger.warning(er)
+		if beginningDate is not None and endingDate is not None:
+			if beginningDate > endingDate:
+				flash('Start Date must be strictly before End Date')
+				return render_template('/prediction/index.html', logged_in=True, meals=mealUsage)
+
+		modelType = 'Polynomial'
+
+		orders = data_controller.get_orders_date_range()
+		#print(list(orders))
+		processedOrders = model_controller.orders_to_list(orders)
+		#print(processedOrders)
+		mainParams = None
+		try:
+			mainParams = model_controller.train_regression(processedOrders, modelType)
+		except RuntimeError:
+			try:
+				mainParams = model_controller.train_regression(processedOrders, 'Sinusoidal')
+			except RuntimeError:
+				flash('Unable to predict meal usage with given data')
+				return render_template('/prediction/index.html', logged_in=True, meals=mealUsage)
+		except TypeError:
+			return render_template('/prediction/index.html', logged_in=True, meals=mealUsage)
+
+		#print(mainParams)
+		mealUsage = prediction_controller.predict_regression(mainParams, modelType, beginningDate, endingDate)
+		print(mealUsage)
+
+	return render_template('/prediction/index.html', logged_in=True, meals=mealUsage)
+
+
