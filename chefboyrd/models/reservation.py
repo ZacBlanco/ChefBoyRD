eb906814616ad9a2fd51537985b75edbfd4f13af
Seobo <seobo11shim@gmail.com>
2017-04-21 21:06:00 -0400

    removed d3 wc. Added responsive wc jQCloud.

--- chefboyrd/models/reservation.py
+++ chefboyrd/models/reservation.py
@@ -16,9 +16,16 @@ class Reservation(UserMixin, BaseModel):
 
     @classmethod
     def cancel_reservation(cls,id):
+        '''Cancels a reservation by id
+        Args: 
+            id(int): Unique Identifier for reservation.
+        
+        Returns:
+            N/A
+        '''
         res = cls.get(cls.id == id)
         res.delete_instance()
-        return
+        return 
 
 
     @classmethod
