4aabf716275372305f10acc70d174db6bab6ba72
Brandon S <HyperBCS@users.noreply.github.com>
2017-04-24 11:43:06 -0400

    Update feedback_controller.py

--- chefboyrd/controllers/feedback_controller.py
+++ chefboyrd/controllers/feedback_controller.py
@@ -1,60 +1,95 @@
-'''
+"""
 This houses all the functions to add or delete sms objects into the database
 Also includes the feedback analysis functions
 
-'''
+"""
 from chefboyrd.models.sms import Sms
-from twilio.rest import TwilioRestClient
+from twilio.rest import Client
 import twilio.twiml
 from peewee import IntegrityError
 from string import punctuation
 from datetime import datetime, date, timedelta
 import configparser
 import os
+from chefboyrd.tests.test_fb_data import test_sms_data, TestMessages
 
-config = configparser.RawConfigParser()
-config.read(os.path.join(os.path.dirname(__file__),'sms.cfg')) #assuming config file same path as this controller
-account_sid = config['keys']['account_sid']
-auth_token = config['keys']['auth_token']
-cust_phone_number = config['test']['cust_phone_number']
-restaurant_phone_number = config['test']['restaurant_phone_number']
+#if in travis, use environment variables. If not in travis, use configuration file. If configuration file missing. email seobo.shim@rutgers.edu
+if '/home/travis/build' in os.path.dirname(__file__):
+    account_sid = os.environ['account_sid']
+    auth_token = os.environ['auth_token']
+else:
+    config = configparser.RawConfigParser()
+    config.read(os.path.join(os.path.dirname(__file__),'sms.cfg')) #assuming config file same path as this controller
+    account_sid = config['keys']['account_sid']
+    auth_token = config['keys']['auth_token']
+    cust_phone_number = config['test']['cust_phone_number']
+    restaurant_phone_number = config['test']['restaurant_phone_number']
 
+Config = configparser.ConfigParser()
+Config.read(os.path.join(os.path.dirname(__file__),"criteriaLists.ini"))
+configDict = {}
+options = Config.options("SectionOne")
+for option in options:
+    try:
+        configDict[option] = Config.get("SectionOne", option)
+    except:
+        configDict[option] = None
 
-def update_db(*date_from):
-    '''
+posWordList = configDict['poslist'].split(' ')
+negWordList = configDict['neglist'].split(' ')
+exceptionWordList = configDict['exceptionlist'].split(' ')
+negationWordList = configDict['negationlist'].split(' ')
+emphasisWordList = configDict['emphasislist'].split(' ')
+foodWordList = configDict['foodlist'].split(' ')
+serviceWordList = configDict['servicelist'].split(' ')
+
+def update_db(*date_from, **update_from):
+    """
     updates the sms in the database starting from the date_from specified (at time midnight)
     no param = updates the sms feedback in database with all message entries
     analyze feedback when sms is sent
-    TODO: this may create duplicate entries, should test this 
     TODO: Fix error with twilio, where the most recent message does not have a submission timep
-    TODO: Fix possible error with DST. TImezone was hotfix
 
     Args:
         date_from (date object): a specified date, where we update db with sms sent after this date
+        update_from: an optional argument. This should be "test" if messages are not coming from
+        twilio, but from the test_fb_data file
     Returns:
         1 on success. 0 on error
     Throws:
-        SystemError: When the Twilio Client cannot be started. Possibly invalid account_sid or auth_token
-    '''
-    try:
-        client = TwilioRestClient(account_sid,auth_token)
-    except:
-        raise SystemError("Could not communicate with Twilio Rest client");
-    if date_from == ():
-        messages = client.messages.list() # this may have a long random string first
+        SystemError: When the Twilio Client cannot be started. Possibly invalid account_sid or
+        auth_token
+    """
+    if update_from:
+        messages = test_sms_data(5, datetime(2016, 3, 25))
     else:
-        date_from = date_from[0]
-        messages = client.messages.list(DateSent=date_from)
+        try:
+            client = Client(account_sid,auth_token)
+        except:
+            raise SystemError
+        #better abstraction would be, twilio function returns a list of objects. this list of
+        #objects is sent to update to update
+        #TODO: check the dates so that it is not greater
+        if date_from == ():
+            messages = client.messages.list() # this may have a long random string first
+        else:
+            date_from = date_from[0]
+            if date_from > datetime.now():
+                #raise ValueError
+                return 0
+            messages = client.messages.list(date_sent=date_from)
     for message in messages:
         try:
-            if (message.date_sent != None):
+            if message.date_sent != None:
                 date_tmp = message.date_sent - timedelta(hours=4)
+                sms_str = date_tmp.strftime("%Y-%m-%d %H:%M:%S")
+                date_tmp= datetime.strptime(sms_str, "%Y-%m-%d %H:%M:%S")
             else:
                 date_tmp = None
             sms_tmp = Sms(
                 sid=message.sid,
-                submission_time= date_tmp,
-                body=message.body, 
+                submission_time=date_tmp,
+                body=message.body,
                 phone_num=message.from_,
                 pos_flag=-1,
                 neg_flag=-1,
@@ -68,11 +103,8 @@ def update_db(*date_from):
             sms_tmp.exception_flag = res2[2]
             sms_tmp.food_flag = res2[3]
             sms_tmp.service_flag = res2[4]
-            #print(sms_tmp.body)
-            #print(sms_tmp.submission_time) 
-            #print(sms_tmp.body)
             err = sms_tmp.save()
-            if not (err):
+            if not err:
                 print("Sms could not be saved in db" + sms_tmp.body)
         except ValueError:
             print("End of messages reached.")
@@ -80,18 +112,21 @@ def update_db(*date_from):
         except IntegrityError:
             err = 0
             #print("Duplicate Sms Entry " + sms_tmp.body)
-            #raise IntegrityError
     return 1 #this should be on success
 
 
 def delete_twilio_feedback():
-    '''
-    wipe all message history on twilio
-    '''
+    """
+    Wipe all message history on twilio
+
+    Raises:
+    	SystemError: Could not communicate with Twilio Rest client
+	ValueError: invalid reference to a stored sms object from the twilio client
+    """
     try:
-        client = TwilioRestClient(account_sid,auth_token)
+        client = Client(account_sid,auth_token)
     except:
-        raise SystemError("Could not communicate with Twilio Rest client");
+        raise SystemError
     messages = client.messages.list()
     for message in messages:
         try:
@@ -111,7 +146,8 @@ def delete_feedback():
     return res
 
 def feedback_analysis(inStr):
-    '''Determines aspects of input string based on word content.
+    """
+    Determines aspects of input string based on word content.
 
     Extended description:
 
@@ -123,12 +159,12 @@ def feedback_analysis(inStr):
         list(posFlag,negFlag,exceptionFlag,foodFlag,serviceFlag):
             A list of integers representing whether the input string
             meets the necessary criteria to be flagged as positive,
-            negative, food-related, service-related or contains an exception
+            negative, food-related, service-related or contains an exception.
 
     Throws:
-        TypeError: When argument is not a string
+        TypeError: When argument is not a string.
         
-    '''
+    """
 
     if not isinstance(inStr, str):
         raise TypeError("Input must be a string")
@@ -139,40 +175,15 @@ def feedback_analysis(inStr):
     foodFlag = 0
     serviceFlag = 0
 
-    Config = configparser.ConfigParser()
-    Config.read(os.path.join(os.path.dirname(__file__),"criteriaLists.ini"))
-    configDict = {}
-    options = Config.options("SectionOne")
-    for option in options:
-        try:
-            configDict[option] = Config.get("SectionOne", option)
-        except:
-            configDict[option] = None
-            
-    posWordList = configDict['poslist']
-    posWordList = posWordList.split(' ')
-    negWordList = configDict['neglist']
-    negWordList = negWordList.split(' ')
-    exceptionWordList = configDict['exceptionlist']
-    exceptionWordList = exceptionWordList.split(' ')
-    negationWordList = configDict['negationlist']
-    negationWordList = negationWordList.split(' ')
-    emphasisWordList = configDict['emphasislist']
-    emphasisWordList = emphasisWordList.split(' ')
-    foodWordList = configDict['foodlist']
-    foodWordList = foodWordList.split(' ')
-    serviceWordList = configDict['servicelist']
-    serviceWordList = serviceWordList.split(' ')
-
+    #config files moved up
     inStrProcessed = inStr
     for p in list(punctuation):
         if p != '\'':
-            inStrProcessed = inStrProcessed.replace(p,' ')
+            inStrProcessed = inStrProcessed.replace(p, ' ')
 
     inStrProcessed = inStrProcessed.lower()
     wordsProcessed = inStrProcessed.split(' ')
-    wordsProcessed = list(filter(bool,wordsProcessed))
-    
+    wordsProcessed = list(filter(bool, wordsProcessed))
     for i, word in enumerate(wordsProcessed):
         if word in posWordList:
             if i > 0:
@@ -184,7 +195,7 @@ def feedback_analysis(inStr):
                             negFlag = 1
                         else:
                             posFlag = 1
-                        
+
                     else:
                         posFlag = 1
             else:
@@ -201,7 +212,7 @@ def feedback_analysis(inStr):
                             posFlag = 1
                         else:
                             negFlag = 1
-                        
+
                     else:
                         negFlag = 1
             else:
@@ -221,7 +232,7 @@ def feedback_analysis(inStr):
         if word in serviceWordList:
             serviceFlag = 1
             break
-            
+
     #print("posFlag = {}:\nnegFlag = {}:\nexceptionFlag = {}:".format(posFlag,negFlag,exceptionFlag),
     #     "\nfoodFlag = {}:\nserviceFlag = {}:".format(foodFlag,serviceFlag))
 
@@ -229,7 +240,8 @@ def feedback_analysis(inStr):
 
 
 def word_freq_counter(inStr):
-    '''Determines frequency of each word in input string.
+    """
+    Determines frequency of each word in input string.
 
     Extended description:
 
@@ -238,24 +250,25 @@ def word_freq_counter(inStr):
                         non-apostrophe punctuation.
 
     Returns:
-        resultDict: A dictionary mapping the distinct words within inStr
-                    to its number of occurrences within the input
+        resultDict: A list of dictionary elements mapping the each distinct word within inStr
+                    to its number of occurrences in the input.
 
     Throws:
-        TypeError: When argument is not a string
+        TypeError: When argument is not a string.
         
-    '''
+    TODO: only get unique words. nothing like( here, restaurant, there, is)
+    """
 
     if not isinstance(inStr, str):
         raise TypeError("Input must be a string")
-    
+
     inStrProcessed = inStr
     for p in list(punctuation):
         if p != '\'':
             inStrProcessed = inStrProcessed.replace(p,' ')
 
     inStrProcessed = inStrProcessed.lower()
-  
+
     wordsProcessed = inStrProcessed.split(' ')
     wordsProcessed = list(filter(bool,wordsProcessed))
     #print("Processed word list: ")
@@ -273,13 +286,12 @@ def word_freq_counter(inStr):
         for  word2 in wordsProcessed:
             if word == word2:
                 freqs[i] = freqs[i] + 1
-                
-    res = []
-    n = 0
-    for n in range(len(wordSet)):
-        res.append(dict(text=wordSet[n],size=freqs[n]))
-    resultDict = res
-    return resultDict
+    try:
+        maxfreq = max(freqs)
+    except ValueError:
+        maxfreq = 0
+    
+    return wordSet, freqs, maxfreq
 
 #muhStr = input("Enter the string: ")
 #dictOut = wordFreqCounter(muhStr)
