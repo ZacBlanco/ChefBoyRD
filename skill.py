fbe9faf419f67e0fff9cb0a3b92f8f47ee60677b
Zac Blanco <zacdblanco@gmail.com>
2017-04-24 20:46:51 -0400

    [Alexa] - moved location of skill and templates to root

--- skill.py
+++ skill.py
@@ -1,7 +1,12 @@
 import logging
+import sys
 from datetime import datetime
 from flask import Flask, render_template
 from flask_ask import Ask, statement, question, session
+
+sys.path.append('..')
+
+from chefboyrd.controllers import data_controller
 
 
 APP = Flask(__name__)
@@ -13,6 +18,8 @@ def reprompt():
     resp._response = {}
     resp._response['directives'] = [{'type':'Dialog.Delegate'}]
     return resp
+
+
 
 @ask.launch
 def welcome():
@@ -33,17 +40,31 @@ def statistics(stat_type, start_date, end_date):
     if start_date is None:
         return reprompt()
 
-    return statement('I got all of your information')
+    msg = 'You wrote bad code'
+    if stat_type is None:
+        pass
+    else:
+        pass
+
+    return statement(msg)
 
 @ask.intent("PredictionIntent", convert={'meal_type': str, 'start_date': datetime, 'end_date': datetime})
 def prediction(meal_type, start_date, end_date):
-
-    if meal_type is None or start_date is None:
+ 
+    if meal_type is None or start_date is None or end_date is None:
         return reprompt()
-
-
-
-    return statement('I got all of your information for prediction.')
+ 
+    modelType = 'Polynomial'
+    orders = data_controller.get_orders_date_range()
+    processedOrders = model_controller.orders_to_list(orders)
+    params = model_controller.train_regression(processedOrders, modelType)
+    mealUsage = prediction_controller.predict_regression(params, modelType, start_date, end_date)
+    if mealUsage is None:
+        return statement(render_template('unsuited_prediction'))
+    for meal_key in mealUsage:
+        if meal_key == meal_type:
+            return statement(render_template('prediction', meal_type=meal_type, meal_count=mealUsage[meal_key]))
+    return statement(render_template('failed_prediction'))
 
 if __name__ == '__main__':
     APP.run(debug=True)
